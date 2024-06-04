770613d108d403766062663771fb0bf04ecb8685
Eddie Sandler <ed.sandler@gmail.com>
2024-06-04 13:20:36 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const apiRouter = require('./routes/routes'); // adjust the path as necessary
+
+app.use(apiRouter);
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
+module.exports=app;
